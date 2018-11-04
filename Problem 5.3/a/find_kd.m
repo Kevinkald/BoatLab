4010e5ca4b81b181147279a663d03e42ab204aaf
Kevin Karlsholm <kevin.k.kaldvansvik@gmail.com>
2018-11-04 17:25:48 +0100

    started 5.3.b

--- Problem 5.3/a/find_kd.m
+++ Problem 5.3/a/find_kd.m
@@ -1,7 +1,11 @@
 %Implementation of the open loop transfer function
 s = tf('s');
-K = 0.1561; T = 72.4391; omega_c = 0.1; Tf = 1/omega_c;
-Kd = 0.90;
+K = 0.1561; T = 72.4391; omega_c = 0.1; 
+
+%PD parameters
+Kd = 0.90; Td = T; Tf = 1/omega_c;
+
+%Transfer function H_0
 H_0 = (K*Kd)/(s*(1+Tf*s));
 
 %Plotting the bode plots of H_0
---
+++ Problem 5.3/b/Plot_response_PD.m
@@ -0,0 +1,25 @@
+filename = '5_3_b.mat';
+m = matfile(filename);
+filedata = m.ans;
+
+time = filedata(1,:);
+psi = filedata(2,:);
+psi_r = filedata(3,:);
+rudder = filedata(4,:);
+
+figure;
+subplot(1,2,1);
+plot(time,psi,'r');
+hold on;
+plot(time,psi_r,'b');
+xlim([0, 500]);
+xlabel('time t [s]');
+ylabel('Angle [deg]');
+legend('\psi(t)','\psi_r(t)');
+
+subplot(1,2,2);
+plot(time,rudder,'r');
+xlim([0, 500]);
+xlabel('time t [s]');
+ylabel('Angle [deg]');
+legend('\delta(t)');
